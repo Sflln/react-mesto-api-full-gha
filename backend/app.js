eed42a1a382086729bde422be4ba5fa42d38e4b3
Sflln <bikbaeva.ns@mail.ru>
2023-10-08 20:26:17 +0300

    15.0

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+
+const limiter = require('./midlewares/rateLimiter');
+
+const routeSignup = require('./routes/signUp');
+const routeSignin = require('./routes/signIn');
+
+const auth = require('./midlewares/auth');
+
+const routeUsers = require('./routes/users');
+const routeCards = require('./routes/cards');
+
+const NotFoundError = require('./errors/NotFoundError');
+const errorHandler = require('./midlewares/errorHandler');
+
+const URL = 'mongodb://0.0.0.0:27017/mestodb';
+const { PORT = 3000 } = process.env;
+
+mongoose.set('strictQuery', true);
+mongoose.connect(URL);
+
+const app = express();
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(limiter);
+
+app.use('/', routeSignup);
+app.use('/', routeSignin);
+
+app.use(auth);
+
+app.use('/users', routeUsers);
+app.use('/cards', routeCards);
+
+app.use((req, res, next) => next(new NotFoundError('Страницы по запрошенному URL не существует')));
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(PORT);
