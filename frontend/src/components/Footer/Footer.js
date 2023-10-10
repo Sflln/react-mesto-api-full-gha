a09832bf4398f85cccaf53605c2a41d221b7eff7
Sflln <121502702+Sflln@users.noreply.github.com>
2023-10-10 08:57:36 +0300

    Update Footer.js

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__copyright">&copy; {new Date().getFullYear()} Борис Зашляпин</p>
+      <p className="footer__copyright">&copy; {new Date().getFullYear()} Safiullina</p>
     </footer>
   );
 };
